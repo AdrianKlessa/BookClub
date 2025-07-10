8440f798387a4ce4d9c0172c3bc68d4d5ea57c27
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-10 16:43:21 +0200

    Make most book fields optional

--- bookclubproject/models.py
+++ bookclubproject/models.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    authors = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
-    avg_rating = models.FloatField()
-    published_date = models.DateField()
-    isbn = models.CharField(max_length=15)
+    description = models.TextField(blank=True)
+    authors = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=100, blank=True)
+    avg_rating = models.FloatField(blank=True, null=True)
+    published_date = models.DateField(blank=True, null=True)
+    isbn = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.title+" by "+self.authors
