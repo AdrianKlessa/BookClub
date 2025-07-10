00723edd5d1055d326b0794fa9abd26cdc809603
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-10 17:01:46 +0200

    Add a subpage to view newly published books

--- bookclubproject/urls.py
+++ bookclubproject/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AllBooks.as_view(), name="index")
+    path("", views.AllBooks.as_view(), name="index"),
+    path("new", views.NewBooks.as_view(), name="new")
 ]
--- bookclubproject/views.py
+++ bookclubproject/views.py
@@ -1,3 +1,5 @@
+from datetime import date as date
+from datetime import timedelta
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -5,4 +7,11 @@ from .models import Book
 
 class AllBooks(ListView):
     model = Book
-    template_name = 'bookclub/index.html'
+    template_name = 'bookclub/book_list.html'
+
+class NewBooks(ListView):
+    model = Book
+    template_name = "bookclub/book_list.html"
+
+    def get_queryset(self):
+        return Book.objects.filter(published_date__gt=date.today() - timedelta(days=30))
