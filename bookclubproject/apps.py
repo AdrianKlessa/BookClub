e6d2f26bc678178cc5987814bfdb6bcbedbab068
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-12 14:35:18 +0200

    Fix automatic profile creation for new users

--- bookclubproject/apps.py
+++ bookclubproject/apps.py
@@ -4,9 +4,5 @@ class BookclubprojectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookclubproject'
 
-
-class UsersConfig(AppConfig):
-    name = 'users'
-
     def ready(self):
-        import signals
+        from . import signals
