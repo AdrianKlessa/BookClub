5702d6a5f26225bf0bdfc26975411043c39f864d
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-12 13:46:09 +0200

    Add tests for book search

--- bookclubproject/tests.py
+++ bookclubproject/tests.py
@@ -17,3 +17,38 @@ class NewBooksViewTest(TestCase):
             old_books.append(create_book("Old Book", date.today() - timedelta(days=365)))
         response = self.client.get(reverse("new"))
         self.assertEqual(list(response.context["book_list"]), new_books)
+
+class BookSearchViewTest(TestCase):
+    def test_search_contains(self):
+        book1 = create_book("Sherlock Holmes and XYZ", date.today())
+        book2 = create_book("Sherlock Holmes and ZYX", date.today())
+        book3 = create_book("Another book", date.today())
+
+        response = self.client.get(reverse('book_search') + '?search=Holmes')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, book1.title)
+        self.assertContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
+
+        books = response.context['book_list']
+        self.assertIn(book1, books)
+        self.assertIn(book2, books)
+        self.assertNotIn(book3, books)
+
+    def test_search_case_insensitive(self):
+        book1 = create_book("Sherlock Holmes and XYZ", date.today())
+        book2 = create_book("Sherlock Holmes and ZYX", date.today())
+        book3 = create_book("Another book", date.today())
+
+        response = self.client.get(reverse('book_search') + '?search=holmes')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, book1.title)
+        self.assertContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
+
+        books = response.context['book_list']
+        self.assertIn(book1, books)
+        self.assertIn(book2, books)
+        self.assertNotIn(book3, books)
