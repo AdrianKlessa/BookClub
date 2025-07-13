523657db0a2ebf87ad6b80255eee24225e97f21d
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-13 15:53:18 +0200

    Add a "add to favorites" button on book view

--- bookclubproject/urls.py
+++ bookclubproject/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("new", views.NewBooks.as_view(), name="new"),
     path("book_details/<int:id>", views.book_details, name="book_details"),
     path("book_search", views.BookSearchView.as_view(), name="book_search"),
+    path("book_details/<int:id>/add_to_favorites/", views.add_to_favorites, name="add_to_favorites"),
 ]
--- bookclubproject/views.py
+++ bookclubproject/views.py
@@ -1,11 +1,12 @@
 from datetime import date as date
 from datetime import timedelta
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
 from django.template import loader
-from .models import Book
+from .models import Book, Profile
 
 class AllBooks(ListView):
     model = Book
@@ -39,3 +40,12 @@ def book_details(request, id):
         'book': book,
     }
     return HttpResponse(template.render(context, request))
+
+@login_required
+def add_to_favorites(request, id):
+    if request.method == "POST":
+        book = get_object_or_404(Book, id=id)
+        profile, created = Profile.objects.get_or_create(user=request.user)
+        if book not in profile.favorite_books.all():
+            profile.favorite_books.add(book)
+    return redirect('book_details', id=id)
