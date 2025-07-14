e823f5c691ecce1b1ca590f5f4db8b70c54aac18
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-14 15:23:44 +0200

    Add the book comment model

--- bookclubproject/admin.py
+++ bookclubproject/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from bookclubproject.models import Book, Profile
+from bookclubproject.models import Book, Profile, BookComment
 
 admin.site.register(Book)
 admin.site.register(Profile)
+admin.site.register(BookComment)
--- bookclubproject/models.py
+++ bookclubproject/models.py
@@ -20,5 +20,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     favorite_books = models.ManyToManyField(Book, related_name='favorited_by', blank=True)
     books_to_read = models.ManyToManyField(Book, related_name='added_to_reading_list_by', blank=True)
+
     def __str__(self):
         return self.user.username
+
+class BookComment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False)
+    published_datetime = models.DateTimeField(auto_now_add=True, blank=True)
+    hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user_profile.user.username+": "+self.comment
