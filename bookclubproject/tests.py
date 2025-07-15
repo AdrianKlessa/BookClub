3e8b6fa3db3a80fdefdde396afe432e62a103b43
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-07-15 16:06:26 +0200

    Add tests for the reading list functionality

--- bookclubproject/tests.py
+++ bookclubproject/tests.py
@@ -109,3 +109,59 @@ class FavoritesTest(TestCase):
 
         response = self.client.get(reverse("favorites"))
         self.assertEqual(list(response.context["book_list"]), [book1, book2])
+
+class ReadingListTest(TestCase):
+    def test_add_to_reading_list(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+
+        book1 = create_book("Sherlock Holmes and XYZ", date.today())
+        book2 = create_book("Sherlock Holmes and ZYX", date.today())
+
+        url = reverse('add_to_reading_list', args=[book1.id])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('book_details', args=[book1.id]))
+
+        profile = Profile.objects.get(user=user)
+        self.assertIn(book1, profile.books_to_read.all())
+        self.assertNotIn(book2, profile.books_to_read.all())
+
+    def test_remove_from_reading_list(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+
+        book1 = create_book("Sherlock Holmes and XYZ", date.today())
+        book2 = create_book("Sherlock Holmes and ZYX", date.today())
+        book3 = create_book("Another book", date.today())
+
+        profile = Profile.objects.get(user=user)
+        profile.books_to_read.add(book1)
+        profile.books_to_read.add(book2)
+
+        url = reverse('remove_from_reading_list', args=[book1.id])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('book_details', args=[book1.id]))
+
+        self.assertNotIn(book1, profile.books_to_read.all())
+        self.assertIn(book2, profile.books_to_read.all())
+        self.assertNotIn(book3, profile.books_to_read.all())
+
+    def test_list_reading_list(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+
+        login = self.client.login(username='testuser', password='12345')
+
+        book1 = create_book("Sherlock Holmes and XYZ", date.today())
+        book2 = create_book("Sherlock Holmes and ZYX", date.today())
+        book3 = create_book("Another book", date.today())
+
+        profile = Profile.objects.get(user=user)
+        profile.books_to_read.add(book1)
+        profile.books_to_read.add(book2)
+
+        response = self.client.get(reverse("reading_list"))
+        self.assertEqual(list(response.context["book_list"]), [book1, book2])
